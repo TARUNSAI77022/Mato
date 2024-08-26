add3e6e3977918cf0cc01be2c4c7749a96419499
Nallajeru Ravi Teja <nallajeruraviteja@outlook.com>
2024-08-26 16:24:23 +0530

    test 5

---
+++ Mato.Service.WebApi/Controllers/TestController5.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mato.Service.WebApi.Controllers
+{
+    public class TestController5 : Controller
+    {
+        public IActionResult Index()
+        {
+            // Example array with 10 elements placed randomly
+            int[] array = { 23, 45, 12, 78, 34, 56, 9, 67, 89, 11 };
+
+            // Sort the array in descending order
+            SortArrayDescending(array);
+
+            // Return the sorted array as JSON
+            return Json(array);
+        }
+
+        private void SortArrayDescending(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        // Swap elements
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
